0694b235f7459d36485abddf1dcd8a4e2baed8de
kobinarth-panchalingam <kobinarth22@gmail.com>
2023-11-29 10:39:04 +0530

    signin and signup page completed

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
